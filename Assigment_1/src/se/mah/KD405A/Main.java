9dbd78e8927be604cfc8c85974977512e0df8327
IvyEnyo <helenaolsson@Helenas-MacBook-Pro.local>
2016-01-21 10:56:19 +0100

    uppdate

--- Assigment_1/src/se/mah/KD405A/Main.java
+++ Assigment_1/src/se/mah/KD405A/Main.java
@@ -6,10 +6,20 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import java.awt.FlowLayout;
+import javax.swing.BoxLayout;
+import javax.swing.JMenuBar;
+import java.awt.List;
+import java.awt.Color;
+import javax.swing.JMenuItem;
+import javax.swing.JTextArea;
+import javax.swing.JList;
+import javax.swing.AbstractListModel;
+import javax.swing.SwingConstants;
+import javax.swing.JTextField;
+import javax.swing.Box;
 
 public class Main extends JFrame {
-
-	private JPanel contentPane;
 
 	/**
 	 * Launch the application.
@@ -31,12 +41,27 @@ public class Main extends JFrame {
 	 * Create the frame.
 	 */
 	public Main() {
+		getContentPane().setBackground(new Color(255, 255, 255));
+		setBackground(Color.BLACK);
+		getContentPane().setForeground(Color.WHITE);
+		getContentPane().setLayout(new BorderLayout(0, 0));
+		
+		JList list = new JList();
+		list.setModel(new AbstractListModel() {
+			String[] values = new String[] {"Andersson, Hanna", "Axelsson, Niklas", "Fredriksson, Håkan ", "Fransson, Irina"};
+			public int getSize() {
+				return values.length;
+			}
+			public Object getElementAt(int index) {
+				return values[index];
+			}
+		});
+		getContentPane().add(list, BorderLayout.WEST);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
-		setContentPane(contentPane);
+		
+		JMenuBar menuBar = new JMenuBar();
+		setJMenuBar(menuBar);
 	}
 
 }
