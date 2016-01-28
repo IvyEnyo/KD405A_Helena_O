880b7736ee5653f0e2354d088a9f141d4a96142a
IvyEnyo <helenaolsson@Helenas-MacBook-Pro.local>
2016-01-28 22:33:13 +0100

    House and houses

---
+++ Assigment_2/src/House.java
@@ -0,0 +1,29 @@
+
+public class House {
+	private int yearBuilt; 
+	private int size; 
+	private final int minSize = 10;
+	private final int maxSize = 1000;
+	
+	public House(int yearBuilt, int size) {
+		this.yearBuilt = yearBuilt;
+		this.size = size;
+	}
+	public int getYearBuilt() {
+		if (this.yearBuilt > 2015) {
+			return 2015;
+		} else if (this.yearBuilt < 1800) {
+			return 1800;
+		}
+		return this.yearBuilt;
+	}
+	
+	public int getSize() {
+		if (this.size > maxSize) {
+			return maxSize;
+		} else if (this.size < minSize) {
+			return minSize;
+		}
+		return size;
+	}
+}
---
+++ Assigment_2/src/HouseGUI.java
@@ -0,0 +1,107 @@
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import java.awt.FlowLayout;
+import java.util.ArrayList;
+import java.util.Random;
+
+import javax.swing.JTextArea;
+import javax.swing.JScrollPane;
+
+
+public class HouseGUI extends JFrame {
+
+	private JPanel contentPane;
+
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					HouseGUI frame = new HouseGUI();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	
+	public HouseGUI() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(6, 6, 438, 266);
+		contentPane.add(scrollPane);
+		
+		JTextArea textArea = new JTextArea();
+		scrollPane.setViewportView(textArea);
+		
+		House house1 = new House(2015, 200);
+		House house2 = new House(1980, 900);
+		House house3 = new House(1900, 2000);
+		
+		
+		System.out.println("Ett hus byggt " + house1.getYearBuilt() + " som är " + house1.getSize() + " kvm stort");
+		System.out.println("Ett hus byggt " + house2.getYearBuilt() + " som är " + house2.getSize() + " kvm stort");
+		System.out.println("Ett hus byggt " + house3.getYearBuilt() + " som är " + house3.getSize() + " kvm stort");
+		
+		textArea.setText("Uppgift 1.2");
+		textArea.setText(textArea.getText()  + "\nEtt hus byggt " + house1.getYearBuilt() + " som är " + house1.getSize() + " kvm stort");
+		textArea.setText(textArea.getText() + "\nEtt hus byggt " + house2.getYearBuilt() + " som är " + house2.getSize() + " kvm stort");
+		textArea.setText(textArea.getText() + "\nEtt hus byggt " + house3.getYearBuilt() + " som är " + house3.getSize() + " kvm stort");
+		
+		House[] myHouses = new House[10];
+		myHouses[0] = new House(1997,500);
+		myHouses[1] = new House(1989,222);
+		myHouses[2] = new House(1987,150);
+		myHouses[3] = new House(2001,790);
+		myHouses[4] = new House(1885,1000);
+		myHouses[5] = new House(1990,999);
+		myHouses[6] = new House(1899,90);
+		myHouses[7] = new House(2004,19);
+		myHouses[8] = new House(2003,5);
+		myHouses[9] = new House(2000,20);
+		
+		textArea.setText(textArea.getText() + "\nUppgift 1.3");
+		
+		for (int i = 0; i < myHouses.length; i++) {
+			textArea.setText(textArea.getText()  + "\nEtt hus byggt " + myHouses[i].getYearBuilt() + " som är " + myHouses[i].getSize() + " kvm stort");
+		}
+		
+		ArrayList<House> myNeighborhood = new ArrayList<House>();
+		
+		textArea.setText(textArea.getText() + "\nUppgift 1.4");
+		
+		Random randomGen = new Random();
+		
+		
+		for (int i = 0; i < 100; i++) {
+			
+			int year = randomGen.nextInt(2015 - 1800) + 1800;
+			int size = randomGen.nextInt(1000 - 10) + 10;
+			
+			myNeighborhood.add(new House(year, size));
+		}
+		
+		for (House house : myNeighborhood) {
+			textArea.setText(textArea.getText()  + "\nEtt hus byggt " + house.getYearBuilt() + " som är " + house.getSize() + " kvm stort");
+		}
+		
+	}
+}
+
