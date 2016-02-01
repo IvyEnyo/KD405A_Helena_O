1ce56237ac3705fc86b3a369391d2f9a3fed1383
IvyEnyo <helenaolsson@Helenas-MacBook-Pro.local>
2016-02-01 14:56:19 +0100

    Bike Class

---
+++ Assigment_3/src/Bike.java
@@ -0,0 +1,35 @@
+
+public class Bike {
+	private String color;
+	private int price;
+	private int size; 
+	
+	public Bike (String color, int size) {
+		this.color = color; 
+		this.size = size; 
+	}
+	public Bike (String color, int size, int price) {
+		this.color = color; 
+		this.size = size; 
+		this.price = price; 
+	}
+	
+	public getColor() {
+
+	}
+	
+	public getSize() {
+		
+	}
+	
+	public getSize() {
+		
+	}
+	
+	public getPrice() {
+		
+	}
+	public setPrice(int price) {
+	
+	}
+}
