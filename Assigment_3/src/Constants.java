e98980c24de0ed21dd87478dc1c45dcdb4537475
IvyEnyo <helenaolsson@Helenas-MacBook-Pro.local>
2016-02-03 22:03:47 +0100

    Array Constants

--- Assigment_3/src/Constants.java
+++ Assigment_3/src/Constants.java
@@ -5,7 +5,7 @@ public class Constants {
 	public static final int MAX_SIZE = 28;
 	public static final int MIN_PRICE = 0;
 	public static final int MAX_PRICE = 30000;
-	public static final String[] bikeColor = {"Black", "Red", "Yellow", "Blue"};
+	public static final String[] bikeColors = {"Black", "Red", "Yellow", "Blue"};
 	
 	
 }
