dbf00a28dbb49a04b3af4e89bcfd893038e3de34
IvyEnyo <helenaolsson@Helenas-MacBook-Pro.local>
2016-02-03 21:50:49 +0100

    BikeTest and Constants

--- Assigment_3/src/BikeTest.java
+++ Assigment_3/src/BikeTest.java
@@ -5,6 +5,19 @@ public class BikeTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		ArrayList<Bike> myBikes = new ArrayList<Bike>();
+		myBikes.add(new Bike("Black", 2000, 15));
+		myBikes.add(new Bike("Black", 2000, 15));
+		myBikes.add(new Bike("Black", 2000, 15));
+		myBikes.add(new Bike("Black", 2000, 15));
+		myBikes.add(new Bike("Black", 2000, 15));
+		myBikes.add(new Bike("Black", 2000, 15));
+		myBikes.add(new Bike("Black", 2000, 15));
+		myBikes.add(new Bike("Black", 2000, 15));
+		myBikes.add(new Bike("Black", 2000, 15));
+		myBikes.add(new Bike("Black", 2000, 15));
 		
+		for(Bike bike: myBikes) {
+			System.out.println(myBikes);
+		}
 	}
 }
---
+++ Assigment_3/src/Constants.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+public class Constants {
+	public static final int MIN_SIZE = 8;
+	public static final int MAX_SIZE = 28;
+	public static final int MIN_PRICE = 0;
+	public static final int MAX_PRICE = 30000;
+	public static final String[] bikeColor = {"Black", "Red", "Yellow", "Blue"};
+	
+	
+}
