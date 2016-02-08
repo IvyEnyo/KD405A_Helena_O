6bf76ba5f3fbec84c8f4180bac40f8d4813393ef
IvyEnyo <helenaolsson@helenas-mbp.clients.internal.mah.se>
2016-02-08 10:52:21 +0100

    varibles and struktured methods

--- Assigment_4B/src/Animal.java
+++ Assigment_4B/src/Animal.java
@@ -1,4 +1,22 @@
 
 public abstract class Animal {
+	private String latinName; 
+	private String friendlyName; 
+	
+	public Animal(String latinName) {
+		// Some code
+	}
+	
+	/*public abstract String getInfo() {
+		// Some code
+	} */
+	
+	public String setFriendlyName(String friendlyName) {
+		return this.friendlyName;
+	}
+	
+	public String getFriendlyName() {
+		return this.friendlyName; 
+	}
 
 }
