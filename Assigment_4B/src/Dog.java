65bfdafedfc485049fbb49bf87d492eddb77adaf
IvyEnyo <helenaolsson@helenas-mbp.clients.internal.mah.se>
2016-02-08 11:15:31 +0100

    Dog class structur

--- Assigment_4B/src/Dog.java
+++ Assigment_4B/src/Dog.java
@@ -1,4 +1,16 @@
 
 public class Dog {
-	private boolean stuip = false; 
+	private boolean stupid;
+	
+	public Dog(String latinName, int gestationTime, boolean stupid) {
+		// Some code
+	}
+	
+	public Dog(String latinName, int gesationTime, boolean stupid, String friendlyName) {
+		// Some code
+	}
+	
+	public boolean isStupid() {
+		return this.stupid; 
+	}
 }
