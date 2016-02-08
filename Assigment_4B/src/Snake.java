93ba7378f6f478d5b92abae7d48cc23072cec2fe
IvyEnyo <helenaolsson@helenas-mbp.clients.internal.mah.se>
2016-02-08 10:59:38 +0100

    structur of Snake.java

--- Assigment_4B/src/Snake.java
+++ Assigment_4B/src/Snake.java
@@ -1,4 +1,17 @@
 
 public class Snake {
+	private int numberOfLifes; 
+	
+	public Cat(String latinName, int gestationTime, int numberOfLifes) {
+		//some code
+	}
+	
+	public int getNumberOfLifes() {
+		return this.numberOfLifes;
+	}
+	
+	public int setNumberOfLifes(int i) {
+		return this.numberOfLifes; 
+	}
 
 }
