e6c3dfb99656f52574525d82991cfa412033a84d
IvyEnyo <helenaolsson@helenas-mbp.clients.internal.mah.se>
2016-02-08 10:56:03 +0100

    structur of Mammal.java

--- Assigment_4B/src/Mammal.java
+++ Assigment_4B/src/Mammal.java
@@ -1,4 +1,13 @@
 
 public abstract class Mammal {
+	private int gestationTime; 
+	
+	public Mammal(String latinName, int gestionTime) {
+		// Some code
+	}
+	
+	public int getGestationtime() {
+		// some code
+	}
 
 }
