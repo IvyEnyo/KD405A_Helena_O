fe8ed4e6865d01478e14513c91685059fb4e15f8
IvyEnyo <helenaolsson@helenas-mbp.clients.internal.mah.se>
2016-02-08 11:11:17 +0100

    Cat class struktur

--- Assigment_4B/src/Cat.java
+++ Assigment_4B/src/Cat.java
@@ -1,4 +1,18 @@
 
 public class Cat {
+	private int numnberOfLifes; 
+	
+	public Cat(String latinName, int gestationTime, int numberOfLifes) {
+		// Some code
+	}
+	
+	public int getNumberOfLifes() {
+		return this.numnberOfLifes;
+	}
+	
+	public int setNumbersOfLifes(int i) {
+		return this.numnberOfLifes;
+		// Some code
+	}
 
 }
