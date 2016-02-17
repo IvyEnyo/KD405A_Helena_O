823688560da3714486a18934816dbfa0aed7cb5d
IvyEnyo <helenaolsson@Helenas-MacBook-Pro.local>
2016-02-17 09:57:54 +0100

    ClockLogic and ClockGUI

--- Assigment_5/src/ClockLogic.java
+++ Assigment_5/src/ClockLogic.java
@@ -1,11 +1,32 @@
+import java.util.Calendar;
 
 public class ClockLogic {
-	private ClockLogic clockGUI; 
+	private DigitalClockGUI clockGUI; 
 	private int alarmHour;
 	private int alarmMinute; 
 	
 	public ClockLogic(DigitalClockGUI digitalClockGUI) {
-		
+		this.clockGUI = digitalClockGUI;
+		ClockThread clockThread = new ClockThread();
+		clockThread.start();
+	}
+	
+	private class ClockThread extends Thread {
+		@Override
+		public void run() {
+			
+			while(true) {
+				try {
+					Thread.sleep(900);
+				} catch (InterruptedException e) {}
+				Calendar cal = Calendar.getInstance();
+				int hours = cal.get(Calendar.HOUR_OF_DAY);
+				int minute = cal.get(Calendar.MINUTE);
+				String hoursString = String.format("%02d", hours);
+				String minuteString = String.format("%02d", minute);
+				clockGUI.setTimeOnLabel(hoursString + ":" + minuteString); 
+			}
+		}
 	}
 	
 	public void setAlarm(int hours, int minute) {
--- Assigment_5/src/DigitalClockGUI.java
+++ Assigment_5/src/DigitalClockGUI.java
@@ -9,16 +9,25 @@ import javax.swing.border.LineBorder;
 import javax.swing.border.TitledBorder;
 
 import java.awt.Window.Type;
+import java.awt.event.ActionEvent;
+
 import javax.swing.JButton;
 import javax.swing.JTextField;
 import javax.swing.JComboBox;
 import javax.swing.JLabel;
 import java.awt.Color;
+import java.awt.Font;
 
 public class DigitalClockGUI extends JFrame {
 
 	private JPanel contentPane;
-
+	private ClockLogic clockLogic;
+	private JLabel labelTime;
+	private JComboBox comboBoxMin;
+	private JComboBox comboBoxHour;
+	private int [] comboHour = {1,2,3,4,5,6,7,8,9,10};
+	private	int [] comboMinute = {1,2,3,4,5,6};
+	
 	/**
 	 * Launch the application.
 	 */
@@ -43,44 +52,75 @@ public class DigitalClockGUI extends JFrame {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 500, 230);
 		contentPane = new JPanel();
+		contentPane.setBackground(new Color(128, 128, 128));
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
 		JButton btnClear = new JButton("clear");
-		btnClear.setBounds(380, 162, 95, 29);
+		btnClear.setBounds(363, 165, 95, 29);
 		contentPane.add(btnClear);
 		
 		JButton btnSet = new JButton("Set");
-		btnSet.setBounds(380, 72, 95, 29);
+		btnSet.setBounds(363, 72, 95, 29);
 		contentPane.add(btnSet);
 		
-		JComboBox comboBoxMin = new JComboBox();
-		comboBoxMin.setBounds(430, 33, 52, 27);
+		
+		comboBoxMin = new JComboBox();
+		comboBoxMin.setBounds(413, 33, 69, 27);
 		contentPane.add(comboBoxMin);
+		comboBoxMin.setAlignmentX(RIGHT_ALIGNMENT);
 		
-		JComboBox comboBoxHour = new JComboBox();
-		comboBoxHour.setBounds(377, 33, 52, 27);
+		for (int i = 0; i < comboMinute.length; i++) {
+			//System.out.println(comboMinute[i]+ "");
+			String stringMinute = String.format("%02d", comboMinute[i]);
+			comboBoxMin.addItem(stringMinute);
+			comboBoxMin.setMaximumRowCount(6);
+
+		}
+		
+		comboBoxHour = new JComboBox();
+		comboBoxHour.setBounds(343, 33, 69, 27);
 		contentPane.add(comboBoxHour);
+		comboBoxHour.setAlignmentX(RIGHT_ALIGNMENT);
 		
-		JLabel labelTime = new JLabel("");
+		for (int i = 0; i < comboHour.length; i++) {
+			//System.out.println(comboHour[i] + "");
+			String stringHour = String.format("%02d", comboHour[i]);
+			comboBoxHour.addItem(stringHour);
+			comboBoxHour.setMaximumRowCount(6);
+		}
+		
+		
+		labelTime = new JLabel("22:00");
+		labelTime.setForeground(new Color(127, 255, 0));
+		labelTime.setFont(new Font("OCR A Std", Font.PLAIN, 36));
+		labelTime.setHorizontalAlignment(JLabel.CENTER);
 		labelTime.setBorder(new BevelBorder(BevelBorder.LOWERED));
-		labelTime.setBounds(64, 56, 258, 72);
+		labelTime.setBounds(60, 48, 258, 72);
 		contentPane.add(labelTime);
+		
 		
 		JLabel labelSetAlarm = new JLabel("");
 		labelSetAlarm.setHorizontalAlignment(JLabel.CENTER);
 		labelSetAlarm.setBorder(new TitledBorder("Alarm"));
-		labelSetAlarm.setBounds(363, 6, 131, 108);
+		labelSetAlarm.setBounds(330, 6, 164, 108);
 		contentPane.add(labelSetAlarm);
 		
 		JLabel labelActiveAlarm = new JLabel("");
 		labelActiveAlarm.setBorder(new TitledBorder("alarm set"));
-		labelActiveAlarm.setBounds(363, 113, 131, 89);
+		labelActiveAlarm.setBounds(330, 113, 164, 89);
 		contentPane.add(labelActiveAlarm);
 		
 		JLabel lableTimeActive = new JLabel("New label");
-		lableTimeActive.setBounds(395, 137, 61, 16);
+		lableTimeActive.setBounds(380, 137, 61, 16);
 		contentPane.add(lableTimeActive);
+		
+		clockLogic = new ClockLogic(this);
+	}
+	
+	public void setTimeOnLabel(String time) {
+		labelTime.setText(time);
 	}
 }
+	
